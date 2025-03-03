43e90f9e785a9c8daef513c8167d469b49485072
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-03-03 23:24:36 +0100

    python312Packages.pytzdata: remove
    Timezone database stuck in 2020. Use pytz or tzdata instead.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13750,8 +13750,6 @@ self: super: with self; {
 
   pytz-deprecation-shim = callPackage ../development/python-modules/pytz-deprecation-shim { };
 
-  pytzdata = callPackage ../development/python-modules/pytzdata { };
-
   pyu2f = callPackage ../development/python-modules/pyu2f { };
 
   pyudev = callPackage ../development/python-modules/pyudev {
