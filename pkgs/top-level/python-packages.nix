063a765ff586351fd2647c616164c7239559e633
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-03-03 06:35:07 +0100

    python313Packages.roman-numerals-py: init at 3.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14512,6 +14512,8 @@ self: super: with self; {
 
   roma = callPackage ../development/python-modules/roma { };
 
+  roman-numerals-py = callPackage ../development/python-modules/roman-numerals-py { };
+
   roman = callPackage ../development/python-modules/roman { };
 
   romy = callPackage ../development/python-modules/romy { };
